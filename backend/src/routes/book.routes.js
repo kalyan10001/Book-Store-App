cb76fbc9169fc387b0edce8c7b029b50c116f6e0
kalyan10001 <venkatakalyan1000@gmail.com>
2025-05-29 16:06:19 +0530

    fetch book routes implemented  --- backend

--- backend/src/routes/book.routes.js
+++ backend/src/routes/book.routes.js
@@ -32,4 +32,30 @@ bookRouter.post("/",protectRoute,async(req,res)=>{
     }
 })
 
+bookRouter.get("/",protectRoute,async(req,res)=>{
+    try {
+        const page=req.query.page || 1;
+        const limit=req.query.limit || 5
+        const skip=(page-1)*limit;
+
+        const books=await Book.find()
+        .sort({createdAt:-1})
+        .skip(skip)
+        .limit(limit)
+        .populate("user","username profileImage");
+
+        const totalBooks=Book.countDocuments();
+
+        res.send({
+            books,
+            currentPage:page,
+            totalBooks,
+            totalPages:Math.ceil(totalBooks/limit),
+        });
+    } catch (error) {
+        console.log("error in fetching books",error);
+        res.status(500).json({message:"internal server error"});
+    }
+})
+
 export default bookRouter;
